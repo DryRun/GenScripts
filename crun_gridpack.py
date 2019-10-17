7223f4c9dbf734371be834a4f2d83c799003a78a
DryRun <david.renhwa.yu@gmail.com>
2019-10-17 17:31:20 -0500

    Add more output saving config, incl EOS

--- crun_gridpack.py
+++ crun_gridpack.py
@@ -11,8 +11,11 @@ if __name__ == "__main__":
 	parser.add_argument("gridpack", type=str, help="Path to gridpack")
 	parser.add_argument("--nevents_job", type=int, default=100, help="Number of events per job")
 	parser.add_argument("--njobs", type=int, default=1, help="Number jobs")
+	parser.add_argument("--keepNano", action='store_true', help="Keep NanoAOD")
+	parser.add_argument("--keepMini", action='store_true', help="Keep MiniAOD")
 	parser.add_argument("--keepDR", action='store_true', help="Keep DR")
-	parser.add_argument("--keepGS", action='store_true', help="Keep DR")
+	parser.add_argument("--keepGS", action='store_true', help="Keep GS")
+	parser.add_argument("--outEOS", type=str, help="Transfer files to EOS instead of back to AFS")
 	args = parser.parse_args()
 
 	# Check gridpack exists
@@ -20,6 +23,11 @@ if __name__ == "__main__":
 	if not os.path.isfile(gridpack_abspath):
 		raise ValueError("Couldn't find gridpack at {}".format(gridpack_abspath))
 	print "Using gridpack at {}".format(gridpack_abspath)
+
+	# For args.outEOS, make sure it's formatted correctly
+	if args.outEOS:
+		if args.outEOS[:6] != "/store":
+			raise ValueError("Argumnet --outEOS must start with /store")
 
 	# Create and move to working directory
 	if os.path.isdir(args.name):
@@ -41,17 +49,30 @@ if __name__ == "__main__":
 		)
 		run_script.write(command + "\n")
 		run_script.write("mv *py $_CONDOR_SCRATCH_DIR\n")
-		run_script.write("mv *MiniAOD*root $_CONDOR_SCRATCH_DIR\n")
-		run_script.write("mv *NanoAOD*root $_CONDOR_SCRATCH_DIR\n")
-		if args.keepDR:
-			run_script.write("mv *DR*root $_CONDOR_SCRATCH_DIR\n")
-		if args.keepGS:
-			run_script.write("mv *GS*root $_CONDOR_SCRATCH_DIR\n")
+
+		if args.outEOS:
+			if args.keepNano:
+				run_script.write("xrdcp *NanoAOD*root root://eoscms.cern.ch//eos/cms/{} \n".format(args.outEOS))
+			if args.keepMini:
+				run_script.write("xrdcp *MiniAOD*root root://eoscms.cern.ch//eos/cms/{} \n".format(args.outEOS))
+			if args.keepDR:
+				run_script.write("xrdcp *DR*root root://eoscms.cern.ch//eos/cms/{} \n".format(args.outEOS))
+			if args.keepGS:
+				run_script.write("xrdcp *GS*root root://eoscms.cern.ch//eos/cms/{} \n".format(args.outEOS))
+		else:
+			if args.keepNano:
+				run_script.write("mv *NanoAOD*root $_CONDOR_SCRATCH_DIR\n")
+			if args.keepMini:
+				run_script.write("mv *MiniAOD*root $_CONDOR_SCRATCH_DIR\n")
+			if args.keepDR:
+				run_script.write("mv *DR*root $_CONDOR_SCRATCH_DIR\n")
+			if args.keepGS:
+				run_script.write("mv *GS*root $_CONDOR_SCRATCH_DIR\n")
 		run_script.write("ls -lrth\n")
 		run_script.write("pwd\n")
 
 	files_to_transfer = [gridpack_abspath, "/afs/cern.ch/user/d/dryu/DAZSLE/gen/scripts/run_gridpack.sh"]
-	csub_command = "csub run.sh -t tomorrow --os SLCern6 -F {} --queue_n {}".format(",".join(files_to_transfer), args.njobs)
+	csub_command = "csub run.sh -t tomorrow -F {} --queue_n {}".format(",".join(files_to_transfer), args.njobs) # --os SLCern6
 	os.system(csub_command)
 
 	os.chdir(cwd)
